1ed5f14410638eebaf450cd2b680f6818b948f2a
rr2105180 <raghad200346@gmail.com>
2024-03-16 23:26:31 +0300

    -

--- project/javascript/script.js
+++ project/javascript/script.js
@@ -1,14 +1,21 @@
-document.getElementById('loginForm').addEventListener('submit', async function(event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const response = await fetch('users.json');
-    const users = await response.json();
-    const find = users.find(user => user.username === username && user.password === password);
-    if (find) {
-        alert('Login successful!');
-        window.location.href = 'index.html';
-    } else {
-        document.getElementById('errorMessage').style.display = 'block';
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById("loginForm");
+    loginForm.addEventListener("submit", async function(event) {
+        event.preventDefault(); 
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+        const response = await fetch("javascript/users.json");
+        const usersData = await response.json();
+        const matchedUser = usersData.find(user => user.username === username && user.password === password);
+        if (matchedUser) {
+             window.location.href = "index.html";
+        } else {
+            alert("Invalid username or password. Please try again.");
+        }
+    });
+    const cancelButton = document.querySelector(".cancel-button");
+    cancelButton.addEventListener("click", function() {
+        document.getElementById("username").value = "";
+        document.getElementById("password").value = "";
+    });
 });
