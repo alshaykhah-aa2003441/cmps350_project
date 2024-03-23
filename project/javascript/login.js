05fa7f55fc1a8b6128d92698e43a63831f55df2c
alshaykhah-aa2003441 <157291998+alshaykhah-aa2003441@users.noreply.github.com>
2024-03-23 11:03:08 +0300

    update

--- project/javascript/login.js
+++ project/javascript/login.js
@@ -1,30 +1,39 @@
 document.addEventListener("DOMContentLoaded", async function() {
     const loginForm = document.getElementById("loginForm");
+
     loginForm.addEventListener("submit", async function(event) {
-        event.preventDefault(); 
+        event.preventDefault();
         const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
-        const response = await fetch("javascript/users.json");
-
+        const balance = document.getElementById("balance").value; 
+        const response = await fetch("users.json");
         const usersData = await response.json();
         
         const matchedUser = usersData.find(user => user.username === username && user.password === password);
         if (matchedUser) {
-             window.location.href = "index.html";
+            matchedUser.balance = balance;
+            await fetch("users.json", {
+                method: "PUT",
+                body: JSON.stringify(usersData),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            window.location.href = "index.html";
         } else {
             alert("Invalid username or password. Please try again.");
         }
-        const user = JSON.parse(usersData)
-        user.push(username, password)
-        fs.writeFileSync('users.json', JSON.stringify(user))
     });
 
     const cancelButton = document.querySelector(".cancel-button");
     cancelButton.addEventListener("click", function() {
         document.getElementById("username").value = "";
         document.getElementById("password").value = "";
+        document.getElementById("balance").value = ""; 
+        document.getElementById("balance").classList.add("hidden"); 
     });
 
-    
+    loginForm.addEventListener("submit", function() {
+        document.getElementById("balance").classList.remove("hidden");
+    });
 });
-
